47c936f83498beeb2e4af432db16973489c2b11d
SegolenePoisson <38692019+SegolenePoisson@users.noreply.github.com>
2019-02-13 14:45:20 +0100

    Update tasks.component.ts

--- Headwork2/src/app/user/tasks/tasks.component.ts
+++ Headwork2/src/app/user/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from '../../data.service';
 import {User} from '../../user';
-import{Task} from '../../task';
+import{Task2} from './create-task';
 
 @Component({
   selector: 'app-tasks',
@@ -9,13 +9,12 @@ import{Task} from '../../task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-
+/*
   datas : any;
   public users : User[];
   public tasks : Task[];
  // public tasks : Task[];
 
-constructor(private dataService : DataService) {
     this.dataService.getEvents().subscribe(
      data => {
        this.users = data
@@ -29,7 +28,7 @@ constructor(private dataService : DataService) {
     
 
    }
-
+*/
   ngOnInit() {
   }
 
